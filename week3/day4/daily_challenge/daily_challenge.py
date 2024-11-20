d0586334c49bd096aa3f04f85f8990c2d33f09af
somerillo <kindadarko@gmail.com>
2024-11-21 00:46:24 +0200

    countries

---
+++ week3/day4/daily_challenge/daily_challenge.py
@@ -0,0 +1,60 @@
+
+    # Using this REST Countries API, create the functionality which will write 10 random countries to your database.
+
+    # These are the attributes which you should populate your tables with: name, capital, flag, subregion, population.
+
+
+import psycopg2
+import requests
+import random
+
+# connect to DB
+connection = psycopg2.connect(database="countries",
+                              user="postgres",
+                              password="postgres_password",
+                              host="localhost",
+                              port="5432")
+
+cursor = connection.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS random_countries") # drop if exists... duh
+create_table_query = f"""CREATE TABLE random_countries (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(100) NOT NULL,
+                        capital VARCHAR(100) NOT NULL,
+                        flag_code VARCHAR(100),
+                        population INTEGER)"""
+cursor.execute(create_table_query)
+connection.commit()
+
+# get data from api rest
+response = requests.get("https://restcountries.com/v3.1/all")
+countries = response.json()
+
+# 10 random countries
+random_countries = random.sample(countries, 10)
+
+# insert countries in DB
+for country in random_countries:
+    name = country['name']['common']
+    capital = country['capital'][0] if 'capital' in country else "no data"
+    flag_code = country['flags']['png'] if 'flags' in country else "no data" # Flagpedia links to svg and png flags
+    population = country['population']
+
+    insert_query = """
+    INSERT INTO random_countries (name, capital, flag_code, population)
+    VALUES (%s, %s, %s, %s)
+    """
+    cursor.execute(insert_query, (name, capital, flag_code, population))
+
+# confirm changes
+connection.commit() # start connection
+cursor.execute("SELECT * FROM random_countries")
+rows = cursor.fetchall()
+print("the random inserted countries are:")
+for row in rows:
+    print(f"{row[0]}- {row[1]},\t\tcapital: {row[2]},\t\tflag code: {row[3]},\tpop: {row[4]}")
+
+# close connection, important!!!! open connection means unused resources
+cursor.close()
+connection.close()
